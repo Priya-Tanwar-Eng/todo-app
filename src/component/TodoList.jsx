f07db5f19b514338054413b9e8ec0caa2ca68316
Priya Tanwar <priya8261717@gmail.com>
2025-11-12 18:37:42 +0530

    Display todo list with conditional rendering and completed task styling

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const TodoList = () => {
+const TodoList = ({state, dispatch}) => {
   return (
-    <div>TodoList</div>
+     <div>
+        <h2>Todo List</h2>
+        {
+            state.length === 0 ? <p>No todos available</p> : null
+        }
+         <ol>
+            {
+              state.map((todo)=>(
+                <li key={todo.id}>
+                    <span style={{textDecoration: todo.complete ? "line-through" : "none"}}>{todo.title}</span>
+                </li>
+              ))
+            }
+         </ol>
+    </div>
   )
 }
 
